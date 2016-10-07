e6b0de877c9bfee449bf4a2567cfd0b9e05ff375
Michael Cruz <michael.cruz@uwalumni.com>
2016-10-07 10:19:08 -0700

    multiple animations added

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 $value = 300
 $location = $('#shooter').offset();
+
+// spaceship constructor function
+function Spaceships(x) {
+  this.x = x
+  this.y = 75
+  this.color = 'rgb(192, 192, 192)'
+  this.draw = function() {
+    ctx.beginPath();
+    ctx.rect(this.x, this.y, 50, 50);
+    ctx.closePath();
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  };
+};
 $(document).ready(function() {
   $(document).keydown(function(e) {
       if (e.keyCode === 37 && $value !== 15) {
@@ -25,33 +39,26 @@ $(document).ready(function() {
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext('2d');
 var raf;
-var switchDirection = [true, true];
+var switchDirection = [true, true, true, true, true];
 
 function alien() {
 if (canvas.getContext) {
 
-// constructor function
-function Spaceships(x) {
-  this.x = x
-  this.y = 75
-  this.color = 'rgb(192, 192, 192)'
-  this.draw = function() {
-    ctx.beginPath();
-    ctx.rect(this.x, this.y, 50, 50);
-    ctx.closePath();
-    ctx.fillStyle = this.color;
-    ctx.fill();
-  };
-};
 var alienOne = new Spaceships(50);
 var alienTwo = new Spaceships(200);
+var alienThree = new Spaceships(350);
+var alienFour = new Spaceships(500);
+var alienFive = new Spaceships(650);
 
 alienOne.draw();
 alienTwo.draw();
+alienThree.draw();
+alienFour.draw();
+alienFive.draw();
 
 setInterval(function redraw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  // left square
+  // first square
   if (alienOne.x == 150) {
     switchDirection[0] = false;
   } else if (alienOne.x == 50) {
@@ -67,7 +74,7 @@ setInterval(function redraw() {
         alienOne.x -= 10;
       }
 
-  // right square
+  // second square
   if (alienTwo.x == 300) {
     switchDirection[1] = false;
   } else if (alienTwo.x == 200) {
@@ -81,6 +88,51 @@ setInterval(function redraw() {
         // ctx.clearRect(alienTwo.x, 100, canvas.width, canvas.height);
         alienTwo.draw();
         alienTwo.x -= 10;
+      }
+  // third square
+  if (alienThree.x == 450) {
+    switchDirection[2] = false;
+  } else if (alienThree.x == 350) {
+    switchDirection[2] = true;
+  }
+      if (switchDirection[2] == true) {
+        // ctx.clearRect(alienTwo.x, 100, canvas.width, canvas.height);
+        alienThree.draw();
+        alienThree.x += 10;
+      } else if (switchDirection[2] == false) {
+        // ctx.clearRect(alienTwo.x, 100, canvas.width, canvas.height);
+        alienThree.draw();
+        alienThree.x -= 10;
+      }
+  // fourth square
+  if (alienFour.x == 600) {
+    switchDirection[3] = false;
+  } else if (alienFour.x == 500) {
+    switchDirection[3] = true;
+  }
+      if (switchDirection[3] == true) {
+        // ctx.clearRect(alienTwo.x, 100, canvas.width, canvas.height);
+        alienFour.draw();
+        alienFour.x += 10;
+      } else if (switchDirection[3] == false) {
+        // ctx.clearRect(alienTwo.x, 100, canvas.width, canvas.height);
+        alienFour.draw();
+        alienFour.x -= 10;
+      }
+  // fifth square
+  if (alienFive.x == 750) {
+    switchDirection[4] = false;
+  } else if (alienFive.x == 650) {
+    switchDirection[4] = true;
+  }
+      if (switchDirection[4] == true) {
+        // ctx.clearRect(alienTwo.x, 100, canvas.width, canvas.height);
+        alienFive.draw();
+        alienFive.x += 10;
+      } else if (switchDirection[4] == false) {
+        // ctx.clearRect(alienTwo.x, 100, canvas.width, canvas.height);
+        alienFive.draw();
+        alienFive.x -= 10;
       }
 }, 250);
 
