d355301eeaec78ca051a641ad5a97f982b498849
Michael Cruz <michael.cruz@uwalumni.com>
2016-10-12 12:05:24 -0700

    improved collision function

--- app.js
+++ app.js
@@ -1,88 +1,9 @@
-$value = 300
-$location = $('#shooter').offset();
-
-// spaceship constructor function
-function Spaceships(x) {
-  this.x = x
-  this.y = 75
-  this.color = 'rgb(192, 192, 192)'
-  this.draw = function() {
-    ctx.beginPath();
-    ctx.rect(this.x, this.y, 50, 50);
-    ctx.closePath();
-    ctx.fillStyle = this.color;
-    ctx.fill();
-  };
-};
-$(document).ready(function() {
-  $(document).keydown(function(e) {
-      if (e.keyCode === 37 && $value !== 15) {
-        $('#shooter').css('left', '' + $value - 15 + 'px');
-        $value -= 15;
-        $location = $('#shooter').offset();
-      } else if (e.keyCode === 39 && $value < 1230) {
-          $('#shooter').css('left', '' + ($value + 15) + 'px');
-          $value = $value + 15;
-          $location = $('#shooter').offset();
-      } else if (e.keyCode === 38) {
-          var laserGun = $("<div id='laser'></div>")
-          $('body').append(laserGun);
-          var laserMove = $('#laser');
-          laserMove.css({top: $location.top, left: $location.left + 18})
-          laserMove.animate({top: '-8px'}, "fast");
-      }
-  })
-});
-
-
-
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext('2d');
-var raf;
-var switchDirection = [true, true, true, true, true];
-var shipParams = [
-  alienOne, alienTwo, alienThree, alienFour, alienFive, alienSix, alienSeven
-];
-
-function alien() {
-if (canvas.getContext) {
-
-var alienOne = new Spaceships(50);
-// var alienTwo = new Spaceships(200);
-var alienThree = new Spaceships(350);
-var alienFour = new Spaceships(500);
-var alienFive = new Spaceships(650);
-
-alienOne.draw();
-alienTwo.draw();
-alienThree.draw();
-alienFour.draw();
-alienFive.draw();
-
-setInterval(function redraw() {
-ctx.clearRect(0, 0, canvas.width, canvas.height);
-var increment += 150;
-for (var j = 0; j < shipParams.length; j++) {
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    if (shipParams[j].x == increment) {
-      switchDirection[0] = false;
-    } else if (shipParams[j].x == (increment - 100)) {
-      switchDirection[0] = true;
-    }
-        if (switchDirection[0] == true) {
-          shipParams[j].draw();
-          shipParams[j].x += 10;
-        } else if (switchDirection[0] == false) {
-          shipParams[j].draw();
-          shipParams[j].x -= 10;
+for (var j = 0; j < aliens.length; j++) {
+      for (var i = 0; i < bullets.length; i++) {
+        if (bullets[i].y < aliens[j].y && bullets[i].x > aliens[j].x && bullets[i].x < aliens[j].x + this.width) {
+          bullets.pop();
+          disappear(j);
+          console.log('boom');
+          };
         }
-  };
-  }, 250);
-
-
-} else {
-  alert('you need a better browser to play this game')
-  }
-};
-alien();
+    }
--- script.js
+++ script.js
@@ -9,7 +9,6 @@ $(document).ready(function() {
     var raf;
     var laser_y = 530;
     var bullets = [];
-
     var switchDirection = [
     true, true, true, true, true, true, true,
     true, true, true, true, true, true, true,
@@ -23,29 +22,29 @@ $(document).ready(function() {
     ];
 
     if (canvas.getContext) {
-        var alienOne = new Spaceships(50, 75, 'rgb(192, 192, 192)', 50, 50, "assets/space-large.png");
-        var alienTwo = new Spaceships(200, 75, 'rgb(192, 192, 192)', 50, 50, "assets/space-large.png");
-        var alienThree = new Spaceships(350, 75, 'rgb(192, 192, 192)', 50, 50, "assets/space-large.png");
-        var alienFour = new Spaceships(500, 75, 'rgb(192, 192, 192)', 50, 50, "assets/space-large.png");
-        var alienFive = new Spaceships(650, 75, 'rgb(192, 192, 192)', 50, 50, "assets/space-large.png");
-        var alienSix = new Spaceships(800, 75, 'rgb(192, 192, 192)', 50, 50, "assets/space-large.png");
-        var alienSeven = new Spaceships(950, 75, 'rgb(192, 192, 192)', 50, 50, "assets/space-large.png");
+        var alienOne = new Spaceships(50, 75, 'rgb(192, 192, 192)', "assets/space-large.png");
+        var alienTwo = new Spaceships(200, 75, 'rgb(192, 192, 192)', "assets/space-large.png");
+        var alienThree = new Spaceships(350, 75, 'rgb(192, 192, 192)', "assets/space-large.png");
+        var alienFour = new Spaceships(500, 75, 'rgb(192, 192, 192)', "assets/space-large.png");
+        var alienFive = new Spaceships(650, 75, 'rgb(192, 192, 192)', "assets/space-large.png");
+        var alienSix = new Spaceships(800, 75, 'rgb(192, 192, 192)', "assets/space-large.png");
+        var alienSeven = new Spaceships(950, 75, 'rgb(192, 192, 192)', "assets/space-large.png");
 
-        var alien1 = new Spaceships(100, 175, 'color', 50, 50, "assets/Space-small-invader.png");
-        var alien2 = new Spaceships(250, 175, 'color', 50, 50, "assets/Space-small-invader.png");
-        var alien3 = new Spaceships(400, 175, 'color', 50, 50, "assets/Space-small-invader.png");
-        var alien4 = new Spaceships(550, 175, 'color', 50, 50, "assets/Space-small-invader.png");
-        var alien5 = new Spaceships(700, 175, 'color', 50, 50, "assets/Space-small-invader.png");
-        var alien6 = new Spaceships(850, 175, 'color', 50, 50, "assets/Space-small-invader.png");
-        var alien7 = new Spaceships(1000, 175, 'color', 50, 50, "assets/Space-small-invader.png");
+        var alien1 = new Spaceships(100, 175, 'color', "assets/Space-small-invader.png");
+        var alien2 = new Spaceships(250, 175, 'color', "assets/Space-small-invader.png");
+        var alien3 = new Spaceships(400, 175, 'color', "assets/Space-small-invader.png");
+        var alien4 = new Spaceships(550, 175, 'color', "assets/Space-small-invader.png");
+        var alien5 = new Spaceships(700, 175, 'color', "assets/Space-small-invader.png");
+        var alien6 = new Spaceships(850, 175, 'color', "assets/Space-small-invader.png");
+        var alien7 = new Spaceships(1000, 175, 'color', "assets/Space-small-invader.png");
 
-        var alienI = new Spaceships(50, 275, 'color', 50, 50, "assets/Space-medium-invader.png");
-        var alienII = new Spaceships(200, 275, 'color', 50, 50, "assets/Space-medium-invader.png");
-        var alienIII = new Spaceships(350, 275, 'color', 50, 50, "assets/Space-medium-invader.png");
-        var alienIV = new Spaceships(500, 275, 'color', 50, 50, "assets/Space-medium-invader.png");
-        var alienV = new Spaceships(650, 275, 'color', 50, 50, "assets/Space-medium-invader.png");
-        var alienVI = new Spaceships(800, 275, 'color', 50, 50, "assets/Space-medium-invader.png");
-        var alienVII = new Spaceships(950, 275, 'color', 50, 50, "assets/Space-medium-invader.png");
+        var alienI = new Spaceships(50, 275, 'color', "assets/Space-medium-invader.png");
+        var alienII = new Spaceships(200, 275, 'color', "assets/Space-medium-invader.png");
+        var alienIII = new Spaceships(350, 275, 'color', "assets/Space-medium-invader.png");
+        var alienIV = new Spaceships(500, 275, 'color', "assets/Space-medium-invader.png");
+        var alienV = new Spaceships(650, 275, 'color', "assets/Space-medium-invader.png");
+        var alienVI = new Spaceships(800, 275, 'color', "assets/Space-medium-invader.png");
+        var alienVII = new Spaceships(950, 275, 'color', "assets/Space-medium-invader.png");
 
         var aliens = [
             alienOne, alienTwo, alienThree, alienFour, alienFive, alienSix, alienSeven,
@@ -67,6 +66,16 @@ $(document).ready(function() {
                 bullet.draw();
             });
 
+            for (var i = 0; i < aliens.length; i++) {
+                for (var j = bullets.length - 1; j >= 0; j--) {
+                    if (bullets[j].y > aliens[i].y && bullets[j].y < aliens[i].y + aliens[i].height && bullets[j].x > aliens[i].x && bullets[j].x < aliens[i].x + aliens[i].width) {
+                        aliens[i].alive = false;
+                        aliens[i] = new Spaceships(-50, -50, 'rgb(192, 192, 192)', "assets/space-large.png");
+                        bullets.splice(j, 1);
+                    }
+                }
+            }
+
             var yOne = 150;
             var xOne = 50;
             for (var i = 0; i < 7; i++) {
@@ -75,10 +84,10 @@ $(document).ready(function() {
                 } else if (aliens[i].x <= xOne) {
                     switchDirection[i] = true;
                 }
-                if (switchDirection[i] == true) {
+                if (switchDirection[i] == true && aliens[i].alive == true) {
                     aliens[i].draw();
                     aliens[i].x += 3/5;
-                } else if (switchDirection[i] == false) {
+                } else if (switchDirection[i] == false && aliens[i].alive == true) {
                     aliens[i].draw();
                     aliens[i].x -= 3/5;
                 }
@@ -94,10 +103,10 @@ $(document).ready(function() {
                 } else if (aliens[i].x <= x1) {
                     switchDirection[i] = true;
                 }
-                if (switchDirection[i] == true) {
+                if (switchDirection[i] == true && aliens[i].alive == true) {
                     aliens[i].draw();
                     aliens[i].x += 1 / 2;
-                } else if (switchDirection[i] == false) {
+                } else if (switchDirection[i] == false && aliens[i].alive == true) {
                     aliens[i].draw();
                     aliens[i].x -= 1 / 2;
                 }
@@ -113,10 +122,10 @@ $(document).ready(function() {
                 } else if (aliens[i].x <= xI) {
                     switchDirection[i] = true;
                 }
-                if (switchDirection[i] == true) {
+                if (switchDirection[i] == true && aliens[i].alive == true) {
                     aliens[i].draw();
                     aliens[i].x += 1 / 3;
-                } else if (switchDirection[i] == false) {
+                } else if (switchDirection[i] == false && aliens[i].alive == true) {
                     aliens[i].draw();
                     aliens[i].x -= 1 / 3;
                 }
@@ -128,14 +137,16 @@ $(document).ready(function() {
         alert('you need a better browser to play this game')
     }
 
-    function Spaceships(x, y, color, width, height, src) {
+    function Spaceships(x, y, color, src) {
         this.x = x
         this.y = y
         this.color = color
-        this.width = width
-        this.height = height
+        this.width = 50
+        this.height = 50
+        this.alive = true
         var image = new Image()
         image.src = src
+
         this.draw = function() {
             ctx.beginPath();
             ctx.drawImage(image, this.x, this.y, this.width, this.height);
@@ -174,7 +185,6 @@ $(document).ready(function() {
         } else if (e.keyCode === 39) {
             rightpressed = true;
         } else if (e.keyCode === 38) {
-            console.log('sup');
             var bullet = new LaserBullet(x_fighter);
             bullets.push(bullet);
         }
